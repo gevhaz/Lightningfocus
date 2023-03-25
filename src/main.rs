d10011c82f0b73c67df1f6913cd8f3419aaf8d78
Simon <gevhaz@tutanota.com>
2023-03-25 16:47:40 +0100

    Move command line flag parsing to args.rs

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,12 @@
+mod args;
+
 use clap::Parser;
 
-#[derive(Parser, Debug)]
-#[command(author, version, about)]
-struct Args {
-    #[arg(short, long, default_value_t=10, value_name="minutes")]
-    work: u8,
-    #[arg(short, long, default_value_t=2, value_name="minutes")]
-    short: u8,
-    #[arg(short, long, default_value_t=5, value_name="minutes")]
-    long: u8,
-}
-
 fn main() {
-    let args = Args::parse();
+    let args = args::Args::parse();
     println!("The work interval is set to {} minutes.", args.work);
     println!("The short break interval is set to {} minutes.", args.short);
     println!("The long break interval is set to {} minutes.", args.long);
-
     loop {
         for _ in 0..4 {
             println!("Work for {} minutes.", args.work);
